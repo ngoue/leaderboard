697a1214abe5d089081e0e577ee0c20e2c81f853
Jordan Gardner <jordanthomasg@gmail.com>
2025-01-14 09:46:47 -0700

    feat: get basic leaderboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,101 +1,95 @@
+"use client";
+
 import Image from "next/image";
+import { useState } from "react";
+import clsx from "clsx";
 
-export default function Home() {
+const playersRaw = [
+  {
+    name: "josh",
+    wins: 1,
+  },
+  {
+    name: "jordan",
+    wins: 2,
+  },
+  {
+    name: "danny",
+    wins: 0,
+  },
+];
+
+export default function Leaderboard() {
+  const [playerState, setPlayerState] = useState(playersRaw);
+
+  const wins = playerState.map(({ wins }) => wins);
+  const maxWins = Math.max(...wins);
+  const minWins = Math.min(...wins);
+  const players = playerState.map((player) => ({
+    ...player,
+    relativeWins: player.wins - minWins,
+    place:
+      player.wins === maxWins && maxWins != 0
+        ? 1
+        : player.wins === minWins
+        ? 3
+        : 2,
+  }));
+
+  console.log({ players });
+
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+    <main className="w-full h-full flex flex-col justify-between items-center">
+      <div>
+        <h1 className="sr-only">Leaderboard</h1>
+      </div>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+      <div className="flex flex-row justify-around items-end w-full px-1 gap-x-1 md:w-auto md:gap-x-8 lg:gap-x-16 xl:gap-x-32">
+        {players.map(({ name, wins, place, relativeWins }) => (
+          <div key={name} className={`${name} flex flex-col max-h-[80vh]`}>
+            <div className="size-32 md:size-36 lg:size-44 xl:size-52">
+              <Image
+                priority
+                alt={`${name} headshot`}
+                src={`/images/${name}.png`}
+                width={256}
+                height={256}
+              />
+            </div>
+            <div
+              className={clsx(
+                "flex flex-col justify-end items-center bg-primary rounded-t min-h-24",
+                place === 1 ? "h-[80vh]" : place === 2 ? "h-[40vh]" : "h-[24vh]"
+              )}
+            >
+              <div className="">
+                <p className="font-extrabold text-black">
+                  {relativeWins > 0 && "+"}
+                  {relativeWins}
+                </p>
+              </div>
+              <div className="flex flex-row gap-x-2 mb-2">
+                <button className="size-12">
+                  <Image
+                    src="/images/caret-up-circle.svg"
+                    alt="caret up circle icon"
+                    width={48}
+                    height={48}
+                  />
+                </button>
+                <button className="size-12">
+                  <Image
+                    src="/images/caret-down-circle.svg"
+                    alt="caret down circle icon"
+                    width={48}
+                    height={48}
+                  />
+                </button>
+              </div>
+            </div>
+          </div>
+        ))}
+      </div>
+    </main>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,7 +8,19 @@ export default {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ["var(--font-crumbl-sans)"],
+      },
       colors: {
+        primary: {
+          DEFAULT: "#FFB9CD", // Pink
+          wcag: "#F79DB7", // Pink, but WCAG compliant for pink-on-white usage
+          light: "#FFE6E5", // Pink Sugar
+        },
+        secondary: {
+          DEFAULT: "#FCEBD9", // Butterscotch Cream
+          light: "#FEF9F3", // Nilla Bean
+        },
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
