23a1c5ee551cff02bd538f0f755a1d7fad4a48be
Jordan Gardner <jordanthomasg@gmail.com>
2025-01-14 11:11:46 -0700

    dope

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,15 @@ import clsx from "clsx";
 
 const playersRaw = [
   {
-    name: "josh",
-    wins: 1,
+    name: "danny",
+    wins: 0,
   },
   {
     name: "jordan",
-    wins: 2,
+    wins: 0,
   },
   {
-    name: "danny",
+    name: "josh",
     wins: 0,
   },
 ];
@@ -23,31 +23,53 @@ export default function Leaderboard() {
   const [playerState, setPlayerState] = useState(playersRaw);
 
   const wins = playerState.map(({ wins }) => wins);
+  const games = wins.reduce((acc, curr) => acc + curr, 0);
   const maxWins = Math.max(...wins);
   const minWins = Math.min(...wins);
   const players = playerState.map((player) => ({
     ...player,
     relativeWins: player.wins - minWins,
-    place:
+    height:
       player.wins === maxWins && maxWins != 0
-        ? 1
+        ? "h-[80vh]"
         : player.wins === minWins
-        ? 3
-        : 2,
+        ? "h-[24vh]"
+        : "h-[40vh]",
   }));
 
-  console.log({ players });
+  const win = (name: string) => {
+    setPlayerState((prev) =>
+      prev.map((player) =>
+        name === player.name ? { ...player, wins: player.wins + 1 } : player
+      )
+    );
+  };
+
+  const lose = (name: string) => {
+    setPlayerState((prev) =>
+      prev.map((player) =>
+        name === player.name
+          ? { ...player, wins: Math.max(player.wins - 1, 0) }
+          : player
+      )
+    );
+  };
 
   return (
     <main className="w-full h-full flex flex-col justify-between items-center">
-      <div>
-        <h1 className="sr-only">Leaderboard</h1>
+      <div className="mt-2">
+        <h1 className="font-extrabold text-black dark:text-primary text-2xl lg:text-4xl">
+          Leaderboard
+        </h1>
       </div>
 
-      <div className="flex flex-row justify-around items-end w-full px-1 gap-x-1 md:w-auto md:gap-x-8 lg:gap-x-16 xl:gap-x-32">
-        {players.map(({ name, wins, place, relativeWins }) => (
-          <div key={name} className={`${name} flex flex-col max-h-[80vh]`}>
-            <div className="size-32 md:size-36 lg:size-44 xl:size-52">
+      <div className="flex flex-row justify-around items-end transition-all w-full px-1 gap-x-1 md:w-auto md:gap-x-8 lg:gap-x-16 xl:gap-x-32">
+        {players.map(({ name, wins, height, relativeWins }) => (
+          <div
+            key={name}
+            className={clsx(name, "flex flex-col max-h-[80vh] transition-all")}
+          >
+            <div className="size-28 md:size-36 lg:size-44 xl:size-52">
               <Image
                 priority
                 alt={`${name} headshot`}
@@ -58,33 +80,45 @@ export default function Leaderboard() {
             </div>
             <div
               className={clsx(
-                "flex flex-col justify-end items-center bg-primary rounded-t min-h-24",
-                place === 1 ? "h-[80vh]" : place === 2 ? "h-[40vh]" : "h-[24vh]"
+                "flex flex-col justify-between items-center bg-primary rounded-t min-h-24 transition-all",
+                height
               )}
             >
-              <div className="">
-                <p className="font-extrabold text-black">
-                  {relativeWins > 0 && "+"}
-                  {relativeWins}
+              <div className="my-4">
+                <p className="font-extrabold text-black text-2xl lg:text-4xl">
+                  {relativeWins > 0 && `+${relativeWins}`}
                 </p>
               </div>
-              <div className="flex flex-row gap-x-2 mb-2">
-                <button className="size-12">
-                  <Image
-                    src="/images/caret-up-circle.svg"
-                    alt="caret up circle icon"
-                    width={48}
-                    height={48}
-                  />
-                </button>
-                <button className="size-12">
-                  <Image
-                    src="/images/caret-down-circle.svg"
-                    alt="caret down circle icon"
-                    width={48}
-                    height={48}
-                  />
-                </button>
+              <div className="flex flex-col justify-between items-center gap-y-4">
+                <div className="flex flex-col justify-between items-center gap-y-2">
+                  <p className="text-black">Wins: {wins}</p>
+                  <p className="text-black">
+                    Rate: {games === 0 ? "-" : Math.trunc((wins / games) * 100)}
+                    %
+                  </p>
+                </div>
+                <div className="flex flex-row gap-x-2 mb-2">
+                  <button className="size-12" onClick={() => win(name)}>
+                    <Image
+                      src="/images/caret-up-circle.svg"
+                      alt="caret up circle icon"
+                      width={48}
+                      height={48}
+                    />
+                  </button>
+                  <button
+                    className="size-12 disabled:opacity-40"
+                    onClick={() => lose(name)}
+                    disabled={wins === 0}
+                  >
+                    <Image
+                      src="/images/caret-down-circle.svg"
+                      alt="caret down circle icon"
+                      width={48}
+                      height={48}
+                    />
+                  </button>
+                </div>
               </div>
             </div>
           </div>
